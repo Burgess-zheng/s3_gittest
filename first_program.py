42929b74dfdfd9968e79df82111c29e5187eb16d
Burgess Zheng <burgess@qq.com>
2018-06-29 16:04:46 +0800

    dev test commit

--- first_program.py
+++ first_program.py
@@ -1,8 +1,7 @@
 def sayhi(name):
     print('hello world' name)
-    change again:
+    change Again: change on dev
     change on linux
     print('second change')
-    change after rollback
     lalala
-
+def dev_test():pass
